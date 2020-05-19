125263623079bd24dc24025310dc15ee770427ef
Irrisor <irrisor@users.noreply.github.com>
2020-05-19 16:02:49 +0200

    header-based php server

--- public/api/util.php
+++ public/api/util.php
@@ -11,7 +11,7 @@ class BadRequestException extends Exception
     }
 }
 
-class TagConflictException extends Exception
+class TagDoesNotMatchException extends Exception
 {
     public function __construct(string $message)
     {
@@ -22,19 +22,14 @@ class TagConflictException extends Exception
 /**
  * Replace contents of a file and return the previous contents.
  * @param $filename string short file name
- * @param $body object {content, expect_tag, tag}
+ * @param $content object
  * @return string old file contents
  * @throws Exception if file cannot be written
  * @throws BadRequestException if the input parameters cannot be used
- * @throws TagConflictException if the expected_tag does not match the file tag
+ * @throws TagDoesNotMatchException if the If-Match header does not match the files tag
  */
-function replaceFileExclusive(string $filename, object $body)
+function replaceFileExclusive(string $filename, object $content, bool $with_metadata = true)
 {
-    $has_metadata = property_exists($body, "content");
-    if ($has_metadata && !property_exists($body, "tag")) {
-        throw new BadRequestException("Can only store content when 'tag' is given.");
-    }
-
     $handle = fopen($filename, "c+");
     if (!$handle) {
         throw new Exception("Failed to open file!");
@@ -44,22 +39,28 @@ function replaceFileExclusive(string $filename, object $body)
         if (flock($handle, LOCK_EX)) {
             try {
                 $previous_body = json_decode(fread($handle, MAX_FILE_SIZE));
-                if ($has_metadata && ($previous_body !== NULL)) {
-                    if (!property_exists($body, "expect_tag")) {
-                        throw new BadRequestException("Can only replace content when 'expect_tag' is given.");
+                if ($with_metadata && $previous_body !== NULL) {
+                    $expected_tag = array_key_exists('If-Match', getallheaders()) ? getallheaders()['If-Match'] : NULL;
+                    if ($expected_tag === NULL) {
+                        throw new BadRequestException("Can only replace content when ETag header is given.");
                     }
-                    if ($previous_body->tag != $body->expect_tag) {
-                        throw new TagConflictException($previous_body->tag);
+                    if ($previous_body->tag != $expected_tag) {
+                        throw new TagDoesNotMatchException($previous_body->tag);
                     }
                 }
 
                 rewind($handle);
-                fwrite($handle, json_encode($has_metadata ? (object)[
-                    "content" => $body->content,
-                    "tag" => $body->tag,
-                ] : $body));
+                if ($with_metadata) {
+                    $etag = hash("sha256", json_encode($content));
+                    header("ETag: $etag");
+                    fwrite($handle, json_encode((object)[
+                        "tag" => $etag,
+                        "content" => $content]));
+                } else {
+                    fwrite($handle, json_encode($content));
+                }
                 ftruncate($handle, ftell($handle));
-                return $previous_body;
+                return $previous_body !== NULL ? $previous_body->content : NULL;
             } finally {
                 flock($handle, LOCK_UN);
             }
--- public/api/write.php
+++ public/api/write.php
@@ -18,24 +18,26 @@ function write(string $path)
         die(1);
     }
     try {
-        ensureAccessOrDie($path, $body);
+        ensureAccessOrDie($path);
 
-        if (property_exists($body, 'content')) {
+        $local_path = "{$data_directory}$path";
+        if (is_file($local_path)) {
             // write contents to a file
             try {
-                replaceFileExclusive("{$data_directory}$path", $body);
+                replaceFileExclusive($local_path, $body);
             } catch (BadRequestException $e) {
                 header("HTTP/1.1 400 Bad Request");
                 print $e->getMessage();
                 die(1);
-            } catch (TagConflictException $e) {
-                header("HTTP/1.1 409 Conflict");
+            } catch (TagDoesNotMatchException $e) {
+                header("HTTP/1.1 412 Precondition Failed");
                 print $e->getMessage();
                 die(1);
             }
         } else if (property_exists($body, 'access')) {
             // create a directory with new access policy
-            replaceFileExclusive("{$data_directory}$path/access.json", $body->access);
+            mkdir($local_path);
+            replaceFileExclusive("{$data_directory}$path/access.json", $body);
             header("HTTP/1.1 201 Created");
         } else {
             header("HTTP/1.1 400 Bad Request");
@@ -51,22 +53,23 @@ function write(string $path)
 
 /**
  * @param string $path string filename to ensure access to (relative to data dir, starts with a slash)
- * @param $body object containing the key property
  * @param bool $write true to check write access (default), false to check read access
  */
-function ensureAccessOrDie(string $path, $body, bool $write = true): void
+function ensureAccessOrDie(string $path, bool $write = true): void
 {
     global $data_directory;
-    $body=(object)$body;
-    if ($body === NULL || !property_exists($body, "key") || !isset($body->key)) {
+    $authorization = array_key_exists('Authorization', getallheaders()) ? getallheaders()['Authorization'] : NULL;
+    if ($authorization === NULL) {
         header("HTTP/1.1 401 Unauthorized");
-        print "No key found to check access to this file.";
+        print "No Authorization header found to check access to this file.";
         die(1);
     }
+    $authorization_exploded = explode(' ', $authorization, 2);
+    $key = count($authorization_exploded) === 2 && $authorization_exploded[0] == "Bearer" ? $authorization_exploded[1] : NULL;
 
     $dir = dirname($path);
     $access_file = "{$data_directory}{$dir}/access.json";
-    if (!file_exists($access_file)) {
+    if (!file_exists($access_file) || $path == $access_file) {
         header("HTTP/1.1 404 Not Found");
         print "Cannot find access policy for '$dir' ($access_file).";
         die(1);
@@ -78,19 +81,34 @@ function ensureAccessOrDie(string $path, $body, bool $write = true): void
         die(1);
     }
     $properties = $write ? ['write_key'] : ['read_key', 'write_key'];
-    if (!array_reduce($properties, function (bool $previous, string $property) use ($access, $body): bool {
-        return $previous || (property_exists($access, $property) && $body->key == $access->$property);
+    if (!array_reduce($properties, function (bool $previous, string $property) use ($access, $key): bool {
+        return $previous || (property_exists($access, $property) && $key == $access->$property);
     }, false)) {
         header("HTTP/1.1 403 Access Denied");
         print "The key is not authorized to write to this file.";
         die(1);
     }
 }
+
 function read(string $path)
 {
     global $data_directory;
-    ensureAccessOrDie($path, $_GET, false);
-    print file_get_contents("{$data_directory}$path");
+    ensureAccessOrDie($path, false);
+    $file_object = json_decode(file_get_contents("{$data_directory}$path"));
+    if (property_exists($file_object, "write_key")) {
+        // don't expose our keys if someone can trick us to read access.json here
+        header("HTTP/1.1 404 Not Found");
+    } else if (property_exists($file_object, "content")) {
+        header("ETag", $file_object->tag);
+        $expected_tag = array_key_exists('If-None-Match', getallheaders()) ? getallheaders()['If-None-Match'] : NULL;
+        if ($expected_tag == $file_object->tag) {
+            header("HTTP/1.1 304 Not Modified");
+        } else {
+            print json_encode($file_object->content);
+        }
+    } else {
+        print json_encode($file_object);
+    }
 }
 
 function create(string $path)
