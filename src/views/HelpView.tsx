11b4fe2bfe69b7616abf89305f700b690a795420
Irrisor <irrisor@users.noreply.github.com>
2020-12-08 22:55:34 +0100

    new help files

--- src/views/HelpView.tsx
+++ src/views/HelpView.tsx
@@ -4,9 +4,10 @@ import {Main} from "../layout/Main";
 export function HelpView() {
     return (<>
         <Main>
-            <object data="/help.pdf" type="application/pdf" style={{width: "100%", height: "100%"}}>
+            <object data="/LernboxKurzanleitung.pdf" type="application/pdf" style={{width: "100%", height: "100%"}}>
                 Hier wird die Lernboxanleitung direkt angezeigt, sofern ein PDF-Plugin im Browser verfügbar ist.
-                Alternativ kann man die <a href="/help.pdf" download>Anleitung herunterterladen</a>.
+                Alternativ kann man die <a href="/LernboxAnleitung.pdf" download>Anleitung</a> oder
+                die <a href="/LernboxKurzanleitung.pdf" download>Kurzanleitung</a> herunterterladen.
             </object>
         </Main>
     </>);
