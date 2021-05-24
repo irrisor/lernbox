fa221fca4fa2bbb98a4c46eb936dc45481656d4c
Irrisor <irrisor@users.noreply.github.com>
2021-05-24 12:33:18 +0200

    unused import

--- src/routes/TeacherRoute.tsx
+++ src/routes/TeacherRoute.tsx
@@ -3,7 +3,6 @@ import {reactContext} from "../data/Context";
 import {Route, Switch, useRouteMatch} from "react-router";
 import {ScreenBox} from "../layout/Screenbox";
 import {TeacherPasswordView} from "../views/PasswordView";
-import {SynchronizationView} from "../views/SynchronizationView";
 import {EditCard} from "../views/EditCard";
 import {CardListView} from "../views/CardListView";
 import {PupilList} from "../views/PupilList";
